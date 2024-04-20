ff54864b6a1ed3cbddcd987f9a77b4b35c9ed8ce
DanThomas64 <5899996+DanThomas64@users.noreply.github.com>
2024-04-19 22:21:24 -0300

    trouble

--- init.lua
+++ init.lua
@@ -342,6 +342,14 @@ vim.keymap.set("n", "<leader>gG", function()
   Util.float_term({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
 end, { desc = "Lazygit (cwd)" })
 
+-- Touble
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
 
 
 -- [[ Configure Treesitter ]]
